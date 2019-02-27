729d33e1c36920df21005a331fff9022ec22ef6b
unknown <amihaidany@gmail.com>
2019-02-27 13:54:50 +0200

    NEW: add new keep component

---
+++ js/apps/keep/cmps/notes-add-cmp.js
@@ -0,0 +1,11 @@
+
+export default {
+    template: `
+                <section class="notes-add">
+                    <input type="text" class="notes-add-input" placeholder="What's on your mind?">
+                </section>`,
+    data() {
+        return {
+        }
+    }
+}
--- js/apps/keep/keep-app-cmp.js
+++ js/apps/keep/keep-app-cmp.js
@@ -1,12 +1,14 @@
 import notesService from './services/notes-service.js'
 import notesList from './cmps/notes-list-cmp.js';
+import notesAdd from './cmps/notes-add-cmp.js';
 
 export default {
     template: `
                 <section class="keep-app">
+                    <notes-add></notes-add>
                     <notes-list :notes="notes"></notes-list>
                 </section>`,
-    components: { notesList },
+    components: { notesList, notesAdd },
     data() {
         return {
             notes: []
